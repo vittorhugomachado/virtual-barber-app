9c97e4e09e3ec5084048682449e7b6083ddf3b79
Vitor Hugo <ovitorhugoalvesmachado@gmail.com>
2026-03-20 19:55:53 -0300

    feat: initial commit

---
+++ vite.config.ts
@@ -0,0 +1,21 @@
+import { defineConfig } from "vite";
+import react, { reactCompilerPreset } from "@vitejs/plugin-react";
+import babel from "@rolldown/plugin-babel";
+import path from "path";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    babel({ presets: [reactCompilerPreset()] }),
+    tailwindcss(),
+  ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
+function tailwindcss(): import("vite").PluginOption {
+  throw new Error("Function not implemented.");
+}
